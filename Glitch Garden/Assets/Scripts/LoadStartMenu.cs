da3aeacb23adfe893c1ac3b56ce959df9572ea1f
Suraj Gupta <48242153+Suraj9300i@users.noreply.github.com>
2020-07-14 17:18:18 +0530

    Day2

---
+++ Glitch Garden/Assets/Scripts/LoadStartMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadStartMenu : MonoBehaviour
+{
+    [SerializeField] float loadingTime = 3f;
+    int current_index;
+    private void Start()
+    {
+        current_index = SceneManager.GetActiveScene().buildIndex;
+        StartCoroutine(WaitForTime());
+    }
+
+    IEnumerator WaitForTime()
+    {
+        yield return new WaitForSeconds(loadingTime);
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(current_index + 1);
+    }
+}
---
+++ Glitch Garden/Assets/Scripts/StartingSound.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartingSound : MonoBehaviour
+{
+    private void Start()
+    {
+        if (FindObjectsOfType(GetType()).Length>1)
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
